e5a6cc0a15c5df6c5bdbe165770fa2eb91c29749
baimau-OwO <56039831+baimau-OwO@users.noreply.github.com>
2019-11-07 19:49:44 +0800

    Add files via upload

---
+++ 107153105作業5.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+class ListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        return
+
+def get_node(count) :
+    print('讓我們將linked list資料列出來')
+    node = node1
+    for i in range (count):
+        print (node.data,end=' ') 
+        node = node.next
+
+def remove_node() :
+    print('讓我們移除第3個資料')
+    node2.next = node3
+
+def insert_node() :
+    print('讓我們在node2和node3之間插入一個數字')
+    print ('node4:',node4.data)
+    node2.next = node4
+    node4.next= node3
+
+def append():
+    print('讓我們放一些資料進linked list')
+    
+    cursor = node1
+    while cursor.next:
+        cursor = cursor.next
+
+    cursor.next = node2
+
+    cursor = node2
+    while cursor.next:
+        cursor = cursor.next
+
+    cursor.next = node3
+    print ('node1:',node1.data)
+    print ('node2:',node2.data)
+    print ('node3:',node3.data)
+    
+if __name__ == "__main__": 
+    print()
+    node1 = ListNode(12) 
+    node2 = ListNode(99) 
+    node3 = ListNode(37)
+    append()
+    print()
+    get_node(3)
+    print()
+    print()
+    node4 = ListNode(8)
+    insert_node()
+    print()
+    get_node(4)
+    print()
+    print()
+    remove_node()
+    print()
+    get_node(3)
