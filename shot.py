51198f453f8c5815e887aae4f157f04d64bd3b84
baimau <55569406+baimau@users.noreply.github.com>
2020-06-18 23:48:11 +0800

    Add files via upload

--- shot.py
+++ shot.py
@@ -1,43 +1,6 @@
 import random, pygame, sys
 from pygame.locals import *
 
-FPS = 30 
-WINDOWWIDTH = 640 
-WINDOWHEIGHT = 480 
-REVEALSPEED = 8
-BOXSIZE = 40 
-GAPSIZE = 10 
-BOARDWIDTH = 4 
-BOARDHEIGHT = 4 
-assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
-XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
-YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
-
-
-GRAY     = (100, 100, 100)
-NAVYBLUE = ( 60,  60, 100)
-WHITE    = (255, 255, 255)
-RED      = (255,   0,   0)
-GREEN    = (  0, 255,   0)
-BLUE     = (  0,   0, 255)
-YELLOW   = (255, 255,   0)
-ORANGE   = (255, 128,   0)
-PURPLE   = (255,   0, 255)
-CYAN     = (  0, 255, 255)
-
-BGCOLOR = NAVYBLUE
-LIGHTBGCOLOR = GRAY
-BOXCOLOR = WHITE
-HIGHLIGHTCOLOR = BLUE
-
-SQUARE = 'square'
-DIAMOND = 'diamond'
-
-
-ALLCOLORS = (RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CYAN)
-ALLSHAPES = (SQUARE, DIAMOND)
-assert len(ALLCOLORS) * len(ALLSHAPES) * 2 >= BOARDWIDTH * BOARDHEIGHT, "Board is too big for the number of shapes/colors defined."
-
 def main():
     global FPSCLOCK, DISPLAYSURF
     pygame.init()
@@ -272,4 +235,60 @@ def hasWon(revealedBoxes):
 
 
 if __name__ == '__main__':
-    main()
+    FPS = 30 
+WINDOWWIDTH = 640 
+WINDOWHEIGHT = 480 
+REVEALSPEED = 8
+BOXSIZE = 40 
+GAPSIZE = 10
+while True:
+    print('1:初級 3*4')
+    print('2:中級 4*4')
+    print('3:高級 5*4')
+    print('')
+    sel = int(input('請選擇:'))
+    print()
+    if sel == 1:
+       BOARDWIDTH = 3
+       BOARDHEIGHT = 4
+       break
+    elif sel == 2:
+         BOARDWIDTH = 4
+         BOARDHEIGHT = 4
+         break
+    elif sel == 3:
+         BOARDWIDTH = 5
+         BOARDHEIGHT = 4
+         break
+    else:
+        print('輸入錯誤,請重新選擇!!')
+        print()
+assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
+XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
+
+
+GRAY     = (100, 100, 100)
+NAVYBLUE = ( 60,  60, 100)
+WHITE    = (255, 255, 255)
+RED      = (255,   0,   0)
+GREEN    = (  0, 255,   0)
+BLUE     = (  0,   0, 255)
+YELLOW   = (255, 255,   0)
+ORANGE   = (255, 128,   0)
+PURPLE   = (255,   0, 255)
+CYAN     = (  0, 255, 255)
+
+BGCOLOR = NAVYBLUE
+LIGHTBGCOLOR = GRAY
+BOXCOLOR = WHITE
+HIGHLIGHTCOLOR = BLUE
+
+SQUARE = 'square'
+DIAMOND = 'diamond'
+
+
+ALLCOLORS = (RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CYAN)
+ALLSHAPES = (SQUARE, DIAMOND)
+assert len(ALLCOLORS) * len(ALLSHAPES) * 2 >= BOARDWIDTH * BOARDHEIGHT, "Board is too big for the number of shapes/colors defined."
+main()
